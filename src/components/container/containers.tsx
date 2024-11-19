0d7ddea7fe7439acca1c9bfabe7223842e5de97d
Nielleeeee <jandnllplz@gmail.com>
2024-11-20 02:52:09 +0800

    chore: format component

--- src/components/container/containers.tsx
+++ src/components/container/containers.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export function MainContainer({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="max-w-[1920px] mx-auto px-4 md:px-8 lg:px-12 xl:px-16 w-full">{children}</div>;
+export function MainContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative z-20 max-w-[1920px] mx-auto px-4 md:px-8 lg:px-12 xl:px-16 w-full">
+      {children}
+    </div>
+  );
 }
