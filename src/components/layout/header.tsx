25231f9cf84063946de953f80a035f0121e53314
Nielleeeee <jandnllplz@gmail.com>
2024-12-25 07:12:53 +0800

    refactor: made the header sticky

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,31 +4,69 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+const navigationList = [
+  { label: "Home", path: "#introduction" },
+  { label: "About", path: "#about" },
+  { label: "Tech Stack", path: "#tech-stack" },
+  { label: "Projects", path: "#projects" },
+  { label: "Experience", path: "#experience" },
+  { label: "Certificates", path: "#certificate" },
+  { label: "Contact", path: "#contact" },
+];
+
 export default function Header() {
   const [isActive, setIsActive] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-  const handleActive = () => {
-    if (isActive) {
-      setIsActive(false);
-    } else {
-      setIsActive(true);
-    }
-  };
+  const handleActive = () => setIsActive(!isActive);
 
   useEffect(() => {
-    if (isActive) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "visible";
-    }
-
-    return () => {
-      document.body.style.overflow = "visible";
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.body.style.overflow = isActive ? "hidden" : "visible";
   }, [isActive]);
 
+  const NavigationItems = () => {
+    return navigationList.map((navigation, index) => (
+      <li
+        key={index}
+        className={`relative group transition-colors duration-300 `}
+        onClick={() => setIsActive(false)}
+      >
+        <Link
+          href={`${navigation.path}`}
+          id={`nav-${navigation.path}`}
+          className=""
+        >
+          {navigation.label}{" "}
+        </Link>
+
+        <span
+          className={`block h-[3px] w-full absolute -bottom-1 left-0 transition-transform duration-300 bg-white scale-x-0 group-hover:scale-x-100`}
+        />
+      </li>
+    ));
+  };
+
   return (
-    <header className="flex flex-row justify-between pt-5 gap-20">
+    <header
+      className={`fixed w-full px-4 lg:px-8 z-30 flex flex-row justify-between py-5 gap-20 bg-transparent transition-all duration-300 ease-in-out ${
+        scrolled ? "backdrop-blur-md bg-black/30 shadow" : "backdrop-blur-none"
+      }`}
+    >
       <div id="logo">
         <Link href={"/"}>
           <Image
@@ -36,55 +74,15 @@ export default function Header() {
             width={1000}
             height={1000}
             alt="logo"
-            className="max-w-full w-[200px] h-auto"
+            className="max-w-full w-40 h-auto"
           />
         </Link>
       </div>
 
       <nav className="flex w-auto">
         {/* Desktop */}
-        <ul className="flex-row gap-6 w-full items-center justify-end text-white text-lg font-medium lg:flex hidden">
-          <li>
-            <Link className="text-white transition group" href="#about">
-              About
-              <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-            </Link>
-          </li>
-
-          <li>
-            <Link className="text-white transition group" href="#tech-stack">
-              Tech Stack
-              <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-            </Link>
-          </li>
-
-          <li>
-            <Link className="text-white transition group" href="#projects">
-              Projects
-              <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-            </Link>
-          </li>
-
-          <li>
-            <Link className="text-white transition group" href="#experience">
-              Experience
-              <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-            </Link>
-          </li>
-
-          <li>
-            <Link className="text-white transition group" href="#certificate">
-              Certificates
-              <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-            </Link>
-          </li>
-
-          <li>
-            <Link className="text-white transition group" href="#contact">
-              Contact
-              <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-            </Link>
-          </li>
+        <ul className="flex-row gap-6 w-full items-center justify-end text-white lg:text-lg font-medium lg:flex hidden">
+          <NavigationItems />
         </ul>
 
         <label className="burger lg:hidden block z-30" htmlFor="burger">
@@ -101,73 +99,9 @@ export default function Header() {
 
         {/* Sidebar */}
         {isActive && (
-          <div className="fixed inset-0 bg-primary z-20 flex justify-center items-center p-8 transform translate-x-0 transition-transform duration-300 ease-in-out">
+          <div className="fixed inset-0 h-screen w-full bg-primary z-20 flex justify-center items-center p-8 transform translate-x-0 transition-transform duration-300 ease-in-out"> 
             <ul className="text-2xl text-center flex flex-col gap-6">
-              <li>
-                <Link
-                  className="text-white transition group"
-                  href="#about"
-                  onClick={() => setIsActive(false)}
-                >
-                  About
-                  <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  className="text-white transition group"
-                  href="#tech-stack"
-                  onClick={() => setIsActive(false)}
-                >
-                  Tech Stack
-                  <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  className="text-white transition group"
-                  href="#projects"
-                  onClick={() => setIsActive(false)}
-                >
-                  Projects
-                  <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  className="text-white transition group"
-                  href="#experience"
-                  onClick={() => setIsActive(false)}
-                >
-                  Experience
-                  <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  className="text-white transition group"
-                  href="#certificate"
-                  onClick={() => setIsActive(false)}
-                >
-                  Certificates
-                  <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-                </Link>
-              </li>
-
-              <li>
-                <Link
-                  className="text-white transition group"
-                  href="#contact"
-                  onClick={() => setIsActive(false)}
-                >
-                  Contact
-                  <div className="w-auto h-[2px] mt-[2px] bg-white transition-all duration-300 opacity-0 group-hover:opacity-100"></div>
-                </Link>
-              </li>
+              <NavigationItems />
             </ul>
           </div>
         )}
