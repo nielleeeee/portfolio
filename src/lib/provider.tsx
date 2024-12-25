c2c5bd3bc9d53f11cc0ce4ceb38b2ed8288a562c
Nielleeeee <jandnllplz@gmail.com>
2024-12-26 01:56:52 +0800

    refactor: added header & footer

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,14 +1,19 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Hotjar from "@/lib/hotjar";
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <>
       <Analytics />
       <SpeedInsights />
       <Hotjar />
+
+      <Header />
       {children}
-    </div>
+      <Footer />
+    </>
   );
 }
