2f51e7aea204718402c73f1db37dcd3e9d0c2381
Nielleeeee <jandnllplz@gmail.com>
2024-12-26 02:18:54 +0800

    chore: added the scroll to top component

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Hotjar from "@/lib/hotjar";
+import ScrollTop from "@/components/scroll/scrollTop";
 import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 
@@ -13,6 +14,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
 
       <Header />
       {children}
+      <ScrollTop />
       <Footer />
     </>
   );
