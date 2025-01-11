1da087226bb2eb71d9c3ae4c0e326006de6e3da8
Nielleeeee <jandnllplz@gmail.com>
2025-01-12 04:08:37 +0800

    chore: format page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import About from "@/components/landingPage/about";
 import Projects from "@/components/landingPage/projects";
 import Experience from "@/components/landingPage/experience";
 import Contact from "@/components/landingPage/contact";
-import { ToastContainer } from "react-toastify";
 import Certificate from "@/components/landingPage/certificate";
+import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -13,12 +13,19 @@ export default function Home() {
   return (
     <main>
       <ToastContainer />
+
       <Introduction />
+
       <About />
+
       <TechStack />
+
       <Projects />
+
       <Experience />
+
       <Certificate />
+
       <Contact />
     </main>
   );
