5fd1042bf7aaad419f72cc704c495da2b1efe339
Nielleeeee <jandnllplz@gmail.com>
2025-01-12 04:08:29 +0800

    chore: added description on meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,28 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import Providers from "@/lib/provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Danielle Portfolio",
+  title: {
+    template: "%s - Danielle Portfolio",
+    default: "Danielle Portfolio",
+  },
+
+  description: "Danielle Portfolio Website",
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    },
+  },
 };
 
 export default function RootLayout({
