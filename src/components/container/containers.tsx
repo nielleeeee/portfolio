0f8d2ba3417302a9ffbfc6a5f3eda8b1feba5e3e
Nielleeeee <jandnllplz@gmail.com>
2025-01-13 00:40:18 +0800

    chore: fix main container class

--- src/components/container/containers.tsx
+++ src/components/container/containers.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function MainContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative z-20 max-w-[1920px] mx-auto px-4 md:px-8 lg:px-12 xl:px-16 w-full">
+    <div className="relative container z-20 mx-auto px-4 md:px-8 lg:px-12 xl:px-16 w-full">
       {children}
     </div>
   );
