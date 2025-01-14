d8691b5ddcd82d78517a8f33b8ba76e86166cb00
Nielleeeee <jandnllplz@gmail.com>
2025-01-15 03:49:27 +0800

    chore: removed unused import

--- src/components/container/containers.tsx
+++ src/components/container/containers.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export function MainContainer({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative container z-20 mx-auto px-4 md:px-8 lg:px-12 xl:px-16 w-full">
