40aa3da8288910e00d3dbb98caf0bcb23cb24991
Nielleeeee <jandnllplz@gmail.com>
2025-01-16 04:05:57 +0800

    chore: changed gif animation

--- src/components/landingPage/experience.tsx
+++ src/components/landingPage/experience.tsx
@@ -65,12 +65,11 @@ export default async function Experience() {
           <div className="hidden md:flex w-1/2">
             <TransitionMoveRight>
               <Image
-                src="/illus-study.gif"
-                width={500}
-                height={500}
+                src="/svg/laptopMan.svg"
+                width={600}
+                height={600}
                 alt="educ"
                 className="w-full rounded-xl backdrop-blur-md"
-                unoptimized
               />
             </TransitionMoveRight>
           </div>
@@ -100,12 +99,11 @@ export default async function Experience() {
           <div className="hidden md:flex w-[40%]">
             <TransitionMoveRight>
               <Image
-                src="/illus-work.gif"
+                src="/svg/laptopCoding.svg"
                 width={500}
                 height={500}
                 alt="educ"
                 className="w-full rounded-xl backdrop-blur-md"
-                unoptimized
               />
             </TransitionMoveRight>
           </div>
@@ -114,3 +112,49 @@ export default async function Experience() {
     </section>
   );
 }
+
+
+{/* <div className="flex h-screen items-center justify-center bg-white px-6 md:px-60">
+  <div className="space-y-6 border-l-2 border-dashed">
+    <div className="relative w-full">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-blue-500">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+      </svg>
+      <div className="ml-6">
+        <h4 className="font-bold text-blue-500">Frontend Development.</h4>
+        <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis ante.</p>
+        <span className="mt-1 block text-sm font-semibold text-blue-500">2007</span>
+      </div>
+    </div>
+    <div className="relative w-full">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-blue-500">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+      </svg>
+      <div className="ml-6">
+        <h4 className="font-bold text-blue-500">Graphic Design.</h4>
+        <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Aliquam tincidunt malesuada tortor vitae iaculis. In eu turpis iaculis, feugiat risus quis, aliquet urna. Quisque fringilla mollis risus, eu pulvinar dolor.</p>
+        <span className="mt-1 block text-sm font-semibold text-blue-500">2007</span>
+      </div>
+    </div>
+    <div className="relative w-full">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-blue-500">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+      </svg>
+      <div className="ml-6">
+        <h4 className="font-bold text-blue-500">Lead Ui/Ux Designer.</h4>
+        <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Aliquam tincidunt malesuada tortor vitae iaculis. In eu turpis iaculis, feugiat risus quis, aliquet urna. Quisque fringilla mollis risus, eu pulvinar dolor</p>
+        <span className="mt-1 block text-sm font-semibold text-blue-500">2007</span>
+      </div>
+    </div>
+    <div className="relative w-full">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-blue-500">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+      </svg>
+      <div className="ml-6">
+        <h4 className="font-bold text-blue-500">Lead Ui/Ux Designer.</h4>
+        <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Aliquam tincidunt malesuada tortor vitae iaculis. In eu turpis iaculis, feugiat risus quis, aliquet urna. Quisque fringilla mollis risus, eu pulvinar dolor</p>
+        <span className="mt-1 block text-sm font-semibold text-blue-500">2007</span>
+      </div>
+    </div>
+  </div>
+</div> */}
