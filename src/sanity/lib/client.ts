acf46e73791a7ba9c7657d155ea0b68f84eca60f
Nielleeeee <jandnllplz@gmail.com>
2025-01-20 01:32:35 +0800

    feat: added sanity fetch

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'next-sanity'
+import { createClient, QueryParams } from 'next-sanity'
 
 import { apiVersion, dataset, projectId } from '../env'
 
@@ -8,3 +8,22 @@ export const client = createClient({
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
+
+export const sanityFetch = async <T>({
+  query,
+  params = {},
+  tags,
+}: {
+  query: string;
+  params?: QueryParams;
+  tags?: string[];
+}): Promise<T> => {
+  const data = await client.fetch(query, params, {
+    next: {
+      revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+      tags,
+    },
+  });
+
+  return data as T;
+};
