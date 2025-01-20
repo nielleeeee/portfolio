d4c507ce56b7848c0fd1021a874184a3386b6150
Nielleeeee <jandnllplz@gmail.com>
2025-01-21 03:18:17 +0800

    chore: dont render header and footer on studio path

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import ScrollTop from "@/components/scroll/scrollTop";
 import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
+import { usePathname } from "next/navigation";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname();
+  const isStudioPath = pathName.startsWith("/studio");
+
   return (
     <>
       <Analytics />
       <SpeedInsights />
 
-      <Header />
+      {!isStudioPath && <Header />}
       {children}
-      <ScrollTop />
-      <Footer />
+      {!isStudioPath && <ScrollTop />}
+      {!isStudioPath && <Footer />}
     </>
   );
 }
