d3467b0a9e3489970d8812f621dece27a55dc062
Nielleeeee <jandnllplz@gmail.com>
2025-01-20 19:59:46 +0800

    chore: used motion react instead of framer motion

--- src/components/animation/transition.tsx
+++ src/components/animation/transition.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 export function TransitionFadeIn({ children }: { children: React.ReactNode }) {
   return (
