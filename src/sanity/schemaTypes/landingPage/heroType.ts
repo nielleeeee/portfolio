df423ab228e9b10cc16e0a1c80da87f87842416e
Nielleeeee <jandnllplz@gmail.com>
2025-01-27 00:01:35 +0800

    feat: created hero type schema

---
+++ src/sanity/schemaTypes/landingPage/heroType.ts
@@ -0,0 +1,39 @@
+import { defineField, defineType } from "sanity";
+
+export const heroType = defineType({
+  name: "heroSection",
+  title: "Hero Section",
+  type: "document",
+  fields: [
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "array",
+      of: [
+        {
+          type: "image",
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "array",
+      of: [{ type: "block" }],
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+});
