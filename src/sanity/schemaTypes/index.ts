a08681743fae26b0810311d8fbc97d62973b2c12
Nielleeeee <jandnllplz@gmail.com>
2025-02-03 01:23:07 +0800

    chore: added types on schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,10 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { heroType } from "@/sanity/schemaTypes/landingPage/heroType";
 import { aboutType } from "@/sanity/schemaTypes/landingPage/aboutType";
+import { techStackType } from "@/sanity/schemaTypes/landingPage/techStackType";
+import { projectType } from "@/sanity/schemaTypes/landingPage/projectType";
+import { websiteSettingsType } from "@/sanity/schemaTypes/websiteSettingsType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [heroType, aboutType],
+  types: [heroType, aboutType, techStackType, projectType, websiteSettingsType],
 };
