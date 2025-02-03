fad0d8274e8c162eb4a6d5387e2b3c8d60c9011a
Nielleeeee <jandnllplz@gmail.com>
2025-02-04 01:19:16 +0800

    chore: created contact type schema

---
+++ src/sanity/schemaTypes/landingPage/contactType.ts
@@ -0,0 +1,23 @@
+import { defineField, defineType } from "sanity";
+
+export const contactType = defineType({
+  name: "contactSection",
+  title: "Contact Section",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+});
