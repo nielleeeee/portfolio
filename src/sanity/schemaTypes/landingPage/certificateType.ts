792e87b73e7073387c9bc8fb008731d71c2f2293
Nielleeeee <jandnllplz@gmail.com>
2025-02-04 01:19:05 +0800

    chore: added certificate type schema

---
+++ src/sanity/schemaTypes/landingPage/certificateType.ts
@@ -0,0 +1,37 @@
+import { defineField, defineType } from "sanity";
+
+export const certificateType = defineType({
+  name: "certificateSection",
+  title: "Certificate Section",
+  type: "document",
+  fields: [
+    defineField({
+      name: "certificateList",
+      title: "Certificate List",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: "name",
+              title: "Name",
+              type: "string",
+              validation: (Rule) => Rule.required(),
+            },
+
+            {
+              name: "image",
+              title: "Image",
+              type: "image",
+              options: {
+                hotspot: true,
+              },
+              validation: (Rule) => Rule.required(),
+            },
+          ],
+        },
+      ],
+    }),
+  ],
+});
