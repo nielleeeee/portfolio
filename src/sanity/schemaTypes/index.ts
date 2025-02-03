ec4c289d33336a24491790f5b9f3c11455a980f2
Nielleeeee <jandnllplz@gmail.com>
2025-02-04 01:19:36 +0800

    chore: added newly created schema

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -4,7 +4,19 @@ import { aboutType } from "@/sanity/schemaTypes/landingPage/aboutType";
 import { techStackType } from "@/sanity/schemaTypes/landingPage/techStackType";
 import { projectType } from "@/sanity/schemaTypes/landingPage/projectType";
 import { websiteSettingsType } from "@/sanity/schemaTypes/websiteSettingsType";
+import { experienceType } from "@/sanity/schemaTypes/landingPage/experienceType";
+import { contactType } from "@/sanity/schemaTypes/landingPage/contactType";
+import { certificateType } from "@/sanity/schemaTypes/landingPage/certificateType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [heroType, aboutType, techStackType, projectType, websiteSettingsType],
+  types: [
+    heroType,
+    aboutType,
+    techStackType,
+    projectType,
+    websiteSettingsType,
+    experienceType,
+    contactType,
+    certificateType,
+  ],
 };
