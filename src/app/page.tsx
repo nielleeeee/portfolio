96415af691466e2385187a3bf704451d43697136
Nielleeeee <jandnllplz@gmail.com>
2025-02-14 03:36:42 +0800

    chore: remove toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,38 @@ import Projects from "@/components/landingPage/projects";
 import Experience from "@/components/landingPage/experience";
 import Contact from "@/components/landingPage/contact";
 import Certificate from "@/components/landingPage/certificate";
-import { ToastContainer } from "react-toastify";
+// import { sanityFetch } from "@/sanity/lib/client";
 
-import "react-toastify/dist/ReactToastify.css";
+// const PAGE_QUERY = `{
+//   "heroSection": *[_type == "heroSection"] {
+//     title,
+//     description,
+//     "imageUrl": image.asset->url,
+//   },
 
-export default function Home() {
+//   "aboutSection": *[_type == "aboutSection"] {
+//     description,
+//     cta,
+//     "image": image.asset->url,
+//   },
+
+//   "techStackSection": *[_type == "techStackSection"] {
+//     title,
+//     techStacks[] {
+//       name,
+//       "logoUrl": logo.asset->url,
+//     }
+//   }
+// }`;
+
+export default async function Home() {
+  // const { heroSection, aboutSection, techStackSection } =
+  //   await sanityFetch<any>({
+  //     query: PAGE_QUERY,
+  //   });
+
   return (
     <main>
-      <ToastContainer />
-
       <Introduction />
 
       <About />
