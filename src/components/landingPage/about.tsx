b908b69713222b22dfc953cef26d68967dc07d11
Nielleeeee <jandnllplz@gmail.com>
2025-02-15 00:56:48 +0800

    chore: added data from sanity

--- src/components/landingPage/about.tsx
+++ src/components/landingPage/about.tsx
@@ -2,20 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { MainContainer } from "@/components/container/containers";
 import { TransitionMoveUp } from "@/components/animation/transition";
-import { performRequest } from "@/lib/datocms";
+import { AboutSection } from "../../../type";
+import { PortableText } from "next-sanity";
 
-const PAGE_CONTENT_QUERY = `
-  query About {
-    about {
-      aboutMe
-      aboutMeLink
-    }
-  }`;
-
-export default async function About() {
-  const {
-    data: { about },
-  } = await performRequest({ query: PAGE_CONTENT_QUERY });
+export default async function About({
+  aboutSectionData,
+}: {
+  aboutSectionData: AboutSection;
+}) {
+  const { description, cta, image } = aboutSectionData;
 
   return (
     <section id="about" className="w-full h-auto bg-white py-10">
@@ -26,7 +21,7 @@ export default async function About() {
             className="flex flex-col md:flex-row justify-evenly items-center gap-4 lg:gap-8"
           >
             <Image
-              src="/computer-seat.gif"
+              src={image as string}
               width={100}
               height={100}
               alt="About Gif"
@@ -35,14 +30,16 @@ export default async function About() {
             />
 
             <div className="w-full md:w-1/2 flex flex-col gap-4 px-4">
-              <p className="font-medium text-justify">{about.aboutMe}</p>
+              <div className="font-medium text-justify">
+                <PortableText value={description} />
+              </div>
 
-              <Link href={about.aboutMeLink} target="_blank" className="w-max">
+              <Link href={cta.slug.current} target="_blank" className="w-max">
                 <button className="learn-more">
                   <span className="circle" aria-hidden="true">
                     <span className="icon arrow"></span>
                   </span>
-                  <span className="button-text">View my projects</span>
+                  <span className="button-text">{cta.name}</span>
                 </button>
               </Link>
             </div>
