cc20915b9912081f7686d3e641f7518b96fe084d
Nielleeeee <jandnllplz@gmail.com>
2025-02-17 01:14:30 +0800

    chore: passed the new project data

--- src/components/list-project/projectList.tsx
+++ src/components/list-project/projectList.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import NewCard from "@/components/cards/newCard";
+import { ProjectSection } from "../../../type";
 
-export default function ProjectList({ projectList }: any) {
+export default function ProjectList({
+  projectData,
+}: {
+  projectData: ProjectSection[];
+}) {
   return (
     <div className="flex flex-wrap flex-row gap-8 w-full justify-center items-center px-0 sm:px-4 md:px-8">
-      {projectList.map((project: any, index: any) => (
-        <NewCard
-          key={index}
-          ProjectLink={project.link}
-          ProjectImage={project.image.responsiveImage}
-          ProjectName={project.name}
-          ProjectDescription={project.description}
-        />
+      {projectData.map((project: ProjectSection, index: number) => (
+        <NewCard key={index} projectData={project} />
       ))}
     </div>
   );
