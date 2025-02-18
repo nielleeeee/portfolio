1aebcf03fbee889dc83ffba80a5dc88ed78f2475
Nielleeeee <jandnllplz@gmail.com>
2025-02-19 03:46:33 +0800

    chore: replace incoming data

--- src/components/slider/certificateSlider.tsx
+++ src/components/slider/certificateSlider.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import Slider, { Settings } from "react-slick";
-import { Image as DatoImage } from "react-datocms";
+import Image from "next/image";
+import { CertificateItem } from "../../../type";
 
-export default function CertificateSlider({ certificateSources }: any) {
+export default function CertificateSlider({
+  certificateItem,
+}: {
+  certificateItem: CertificateItem[];
+}) {
   const settings: Settings = {
     dots: true,
     infinite: true,
@@ -13,22 +18,28 @@ export default function CertificateSlider({ certificateSources }: any) {
     swipeToSlide: true,
     autoplay: true,
     autoplaySpeed: 8000,
+    draggable: true,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    focusOnSelect: true,
+    className: "w-full",
   };
 
   return (
     <div className="px-5 sm:px-10">
       <Slider {...settings}>
-        {certificateSources.map((certificate: any, index: any) => (
+        {certificateItem.map((certificate: CertificateItem, index: any) => (
           <div
             key={index}
-            className="w-full px-3 md:px-5 mx-auto flex justify-center items-center overflow-hidden"
+            className="cursor-pointer w-full px-3 md:px-5 mx-auto flex justify-center items-center overflow-hidden"
           >
-            <DatoImage
-              data={certificate.image.responsiveImage}
-              pictureClassName={
-                "object-contain w-full max-w-4xl mx-auto aspect-[4/3] text-center rounded-xl"
-              }
-              className="!max-w-none"
+            <Image
+              src={certificate.image as string}
+              alt={certificate.name}
+              width={800}
+              height={800}
+              draggable={false}
+              className="object-contain w-full max-w-4xl mx-auto aspect-[4/3] text-center rounded-xl"
             />
           </div>
         ))}
