f24d7cd79256f8c8489e48b28f909769b9d64da5
Nielleeeee <jandnllplz@gmail.com>
2025-02-19 03:46:15 +0800

    chore: remove datocms data

--- src/components/landingPage/certificate.tsx
+++ src/components/landingPage/certificate.tsx
@@ -1,34 +1,14 @@
 import { TransitionMoveUp } from "@/components/animation/transition";
 import { MainContainer } from "@/components/container/containers";
 import CertificateSlider from "@/components/slider/certificateSlider";
-import { performRequest } from "@/lib/datocms";
+import { CertificateSection } from "../../../type";
 
-const PAGE_CONTENT_QUERY = `
-  query Certificate {
-    allCertificates {
-      name
-      image {
-        url
-        responsiveImage {
-          srcSet
-          webpSrcSet
-          sizes
-          src
-          width
-          height
-          aspectRatio
-          alt
-          title
-          base64
-        }
-      }
-    }
-  }`;
-
-export default async function Certificate() {
-  const {
-    data: { allCertificates },
-  } = await performRequest({ query: PAGE_CONTENT_QUERY });
+export default async function Certificate({
+  certificateData,
+}: {
+  certificateData: CertificateSection;
+}) {
+  const { title, certificateList } = certificateData;
 
   return (
     <section
@@ -39,10 +19,10 @@ export default async function Certificate() {
         <TransitionMoveUp>
           <div className="flex flex-col justify-center gap-4 lg:gap-8 z-10">
             <h3 className="font-bold text-3xl text-secondary text-center">
-              Certificates
+              {title}
             </h3>
 
-            <CertificateSlider certificateSources={allCertificates} />
+            <CertificateSlider certificateItem={certificateList} />
           </div>
         </TransitionMoveUp>
       </MainContainer>
