c5b700f82bd61575a0f635a5ca9809645a2df3de
nielleeeee <jandnllplz@gmail.com>
2025-05-23 02:04:40 +0800

    chore: remove search params import

--- src/components/pageTracker/pageTracker.ts
+++ src/components/pageTracker/pageTracker.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { PageData } from "../../../type";
 import { sendPageData } from "@/app/action/sendPageData";
 
